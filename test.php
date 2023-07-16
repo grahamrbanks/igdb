f130f5d11753a2a895a1d3f625c5dac8d06ad423
grahamrbanks <139666042+grahamrbanks@users.noreply.github.com>
2023-07-16 17:32:06 +1000

    Update test.php

--- test.php
+++ test.php
@@ -2,16 +2,14 @@
 
     require_once "src/class.igdb.php";
 
-    $builder = new IGDBQueryBuilder();
+    $igdb = new IGDB("ujrqcn26y62avuwvwx06cgtsjw4swa", "z9f0h3yuynx87d89h0zd913ezfiqps");
 
     try {
-        $query = $builder
-            ->search("uncharted 4")
-            ->fields("id, name")
-            ->limit(1)
-            ->build();
-    } catch (IGDBInvalidParameterException $e) {
-        // Invalid argument passed to one of the methods
+        $result = $igdb->game('search "uncharted 4"; fields id,name; limit 1;');
+    } catch (IGDBEndpointException $e) {
+        // Something went wrong, we have an error
         echo $e->getMessage();
     }
+
 ?>
+
